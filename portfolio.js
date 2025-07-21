a94a95197f43fc6a574bc083f1d22fe987331598
bhumika8527 <bhumika8527@gmail.com>
2025-07-21 23:25:54 +0530

    make changes

--- portfolio.js
+++ portfolio.js
@@ -13,3 +13,28 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 }
 
+//  sidemenu
+var sidemenu=document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right="0";
+
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+
+}
+
+ 
+    const scriptURL =" https://script.google.com/macros/s/AKfycbwSjbepJo4vUIKK9V4PhsecFQaATA7Wu9IIvlAZvhAx4_dENpj1-S-ddAANAhPxrSds/exec"
+  const form = document.forms['submit-to-google-sheet']
+ const msg=document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{msg.innerHTML="Mssage sent successfully"})
+      setTimeout(function(){
+        msg,innerHTML=""
+      },5000)
+      form.reset()
+      .catch(error => console.error('Error!', error.message))
+  })
