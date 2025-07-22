08456be9037857282088c5a0a5d2b8fce44726f0
bhumika8527 <bhumika8527@gmail.com>
2025-07-22 20:52:03 +0530

    make changes

--- portfolio.js
+++ portfolio.js
@@ -25,16 +25,32 @@ function closemenu(){
 }
 
  
-    const scriptURL =" https://script.google.com/macros/s/AKfycbwSjbepJo4vUIKK9V4PhsecFQaATA7Wu9IIvlAZvhAx4_dENpj1-S-ddAANAhPxrSds/exec"
-  const form = document.forms['submit-to-google-sheet']
- const msg=document.getElementById("msg")
+//     const scriptURL ="https://script.google.com/macros/s/AKfycbwSjbepJo4vUIKK9V4PhsecFQaATA7Wu9IIvlAZvhAx4_dENpj1-S-ddAANAhPxrSds/exec"
+//   const form = document.forms['submit-to-google-sheet']
+//  const msg=document.getElementById("msg")
+//   form.addEventListener('submit', e => {
+//     e.preventDefault()
+//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+//       .then(response =>{msg.innerHTML="Message sent successfully"})
+//       setTimeout(function(){
+//         msg,innerHTML=""
+//       },5000)
+//       form.reset()
+//       .catch(error => console.error('Error!', error.message))
+//   })
+ const scriptURL ="https://script.google.com/macros/s/AKfycbxCOcmi1RiAulVEvy-5Ej7TtiZ83ib4QpSePRStqK0T9LumlDi9jFdqnuD8tI_E08Ov/exec"
+  const form = document.forms["submit-to-google-sheet"]
+  const msg=document.getElementById("msg")
+
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response =>{msg.innerHTML="Mssage sent successfully"})
-      setTimeout(function(){
-        msg,innerHTML=""
-      },5000)
-      form.reset()
+      .then(response =>{
+        msg.innerHTML="Message sent successfully"
+       setTimeout(function(){
+        msg.innerHTML=""
+       },5000)
+       form.reset()
+      })
       .catch(error => console.error('Error!', error.message))
   })
